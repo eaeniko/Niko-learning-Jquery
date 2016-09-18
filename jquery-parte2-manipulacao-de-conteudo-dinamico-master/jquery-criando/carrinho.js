56a00eae8ce262a204ada5884d301f870344f781
NikolaiCinotti <agencianiko@hotmail.com>
2016-09-18 13:16:38 -0300

    seletores e correção de bugs

--- jquery-parte2-manipulacao-de-conteudo-dinamico-master/jquery-criando/carrinho.js
+++ jquery-parte2-manipulacao-de-conteudo-dinamico-master/jquery-criando/carrinho.js
@@ -1,6 +1,7 @@
 var atualizaDados = function () {
+
 	var total = 0;
-	var itens = $('.item-total');
+	var itens = $('.item-total:visible');
 	for (var i = 0; i < itens.length; i++) {
 		var item = $(itens[i]);
 		var valor = parseFloat(item.text());
@@ -9,17 +10,30 @@ var atualizaDados = function () {
 	console.log("Valor total: " + total);
 	$('#valor-total').text(total);
 	$('#quantidade-item').text(itens.length);
-	};
+};
 
-	var removeItem = function(event) {
+var removeItem = function(event) {
+
 	event.preventDefault();
 	var self = $(this);
-	self.closest('tr').remove();
+	self.closest('tr').hide();
 
 	atualizaDados();
-	};
-	var aposInicializado = function() {
+};
+
+var undo = function () { 
+	$("tr:visible").removeClass('recuperado');
+
+	var trs = $('tr:hidden');
+	trs.addClass('recuperado').show();
+
+
+};
+
+var aposInicializado = function() {
+
 	atualizaDados();
+	$('#undo').click(undo);
 	$('.remove-item').click(removeItem);
 	};
 
